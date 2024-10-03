48618709ae787bec1939225281273a5088592fb0
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-03 22:37:18 +0530

    done frameworks app grid

--- Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
+++ Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     
+    @StateObject var viewModal = FrameworkGridViewModal()
+    
     let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
@@ -17,32 +19,22 @@ struct FrameworkGridView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
-                            .preferredColorScheme(.dark) // System Color Themes
+                            .preferredColorScheme(.dark)
+                            .onTapGesture {
+                                viewModal.selectedFramework = framework
+                            }
                     }
                 }
             }
             .navigationTitle("🍎 Frameworks")
+            .sheet(isPresented: $viewModal.isShowingDetailView) {
+                FrameWorkView(framework: viewModal.selectedFramework ?? MockData.sampleFrameWork,
+                              isShowingDetailView: $viewModal.isShowingDetailView)
+            }
         }
     }
 }
 
 #Preview {
     FrameworkGridView()
-}
-
-struct FrameworkTitleView: View {
-    let framework: Framework
-    
-    var body: some View {
-        VStack {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.5) // Size / width of the text (50% here)
-        }
-    }
 }
